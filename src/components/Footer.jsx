39b7d03668a9b53c3f3f04779b3d64da0f9c6aae
sayan <sayanmandal8643@gmail.com>
2021-08-07 17:28:56 +0530

    git commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Footer() {
+
+    const CurrentYear = new Date().getFullYear();
+
+
+
+
+
+    return (
+        <footer>
+            <p>
+                Copyright © {CurrentYear}
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
